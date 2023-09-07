f78b55969ec482511eaff617cb9b5f45ae63a847
Kanimozhi26596 <kanikannan26@gmail.com>
2023-09-07 10:24:10 +0530

    Generate Json to Html task completed

--- index.js
+++ index.js
@@ -1,40 +1,44 @@
-var inputText;
+var inputJsonData;
 function inputElements(){
-  inputText = document.getElementById("inputText").value;
-  inputText = JSON.parse(inputText);
-  return inputText
-   };
-   function generateButton(){
+  inputJsonData = document.getElementById("inputText").value;
+  inputJsonData = JSON.parse(inputJsonData);
+  return inputJsonData;
+   }
+   function generateHtml(){
 
-    const outputText = document.getElementById("outputText");
+    let outputText = document.getElementById("outputText");
+    outputText.innerHTML = " ";
     var uniqueId = {};
-       
-      inputText.forEach((ele)=>{
-       if(uniqueId === "" ){
-        alert("plese enter valid input ")
-       }
-       else if(!uniqueId[ele.id]){ 
-        const rootNode = document.createElement('div');
-        rootNode.classList = "rootNode";
-        rootNode.setAttribute("data-id", ele.id);
-        const username = document.createElement('span');
-        username.classList = "userName";
-        rootNode.appendChild(username);
-        const video_name = document.createElement('span');
-        video_name.classList = "video_name";
-        rootNode.appendChild(video_name);
-        const additional_details = document.createElement('span');
-        additional_details.classList = "additional_Details";
-        const type = document.createElement('span');
-        type.classList = "type";
-        additional_details.appendChild(type);
-        rootNode.appendChild(additional_details);
-        outputText.innerHTML += rootNode.outerHTML;
-        uniqueId[ele.id] = true;       
-      }
-       else{
-        alert("Duplicate Id");
-       }
-      })
-     
+
+      inputJsonData.forEach((ele)=>{
+        if(!uniqueId[ele.id]){ 
+          let rootNode = document.createElement('div');
+          rootNode.classList = "rootNode";
+          rootNode.setAttribute("data-id", ele.id);
+          let additional_details = document.createElement('div');
+          additional_details.setAttribute('class','additional_details');
+            
+            const keys = Object.keys(ele);
+            for (const key of keys) {
+                if(key != 'id' && key == 'username' || key == 'video_name'){
+                    let span = document.createElement('span');
+                    span.setAttribute('class',key);
+                    span.innerText = ele[key];
+                    rootNode.appendChild(span);
+                } else if(key != 'id') {
+                    let span = document.createElement('span');
+                    span.setAttribute('class',key);
+                    span.innerText = ele[key];
+                    additional_details.appendChild(span);
+                }
+            }
+          rootNode.appendChild(additional_details);
+          outputText.innerHTML += rootNode.outerHTML;
+          uniqueId[ele.id] = true;       
+        }
+        else{
+          alert("Duplicate Id");
+          outputText.innerText = " ";
+        }
+      });
   }  
